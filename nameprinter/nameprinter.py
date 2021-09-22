d07a335eeae2e769b26df830ab5287b8900abcda
cnelson20 <cjnelson2004@gmail.com>
2021-09-22 09:26:38 -0400

    prints one name randomly

--- nameprinter/nameprinter.py
+++ nameprinter/nameprinter.py
@@ -1,14 +1,19 @@
+import random 
+
+pd1 = [];
+pd2 = [];
+
 f = open("names.txt");
 text = f.read();
 lines = text.split("\n");
-i = 1;
-while (i < len(lines)):
-    curLine = (lines[i]);
-    lineSplit = lines[i].split(" ");
-    toPrint = "";
-    if (len(lineSplit) > 4):
-        toPrint = toPrint + lineSplit[4];
-        if (len(lineSplit) > 5):
-            toPrint = toPrint + " " + lines[i].split(" ")[5];
+i = random.randint(0,int(len(lines)/2))*2-1;
+curLine = (lines[i]);
+
+lineSplit = lines[i].split(" ");
+toPrint = "";
+if (len(lineSplit) > 4):
+    toPrint = toPrint + lineSplit[4];
+    if (len(lineSplit) > 5):
+        toPrint = toPrint + " " + lines[i].split(" ")[5];
         print (toPrint);
-    i = i + 2;
+
