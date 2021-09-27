1f5786b5a59d7998b609e45b6c304bc54523ddc7
cnelson20 <cjnelson2004@gmail.com>
2021-09-27 09:16:57 -0400

    updated list of names

--- 05_py/main.py
+++ 05_py/main.py
@@ -6,8 +6,12 @@
 import random
 
 # List people for periods 1 and 2
-pd1 = ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Ishraq Mahid", "Raymond Yeung", "Kevin Cao", "Ivan Lam", "Michelle Lo"]
-pd2 = ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee", "Patrick Ging", "Yaying Liang", "Shadman Rakib", "Cameron Nelson", "Eric Guo", "Daniel Sooknanen", "Andy Lin"]
+pd1 = [    "Owen Yaggy", "Haotian Gan", "Ishraq Mahid", "Ivan Lam", "Michelle Lo","Christopher Liu", "Ivan Mijacika", "Lucas Lee", "Josephine Lee", "Rayat Roy", "Emma Buller", "William Chen", "Rachel Xiao", "Andrew Juang", "Yuqing Wu", "Renggeng Zheng", "Annabel Zhang", "Alejandro Alonso", "Deven Maheshwari", "Oscar Wang", "Tami Takada", "Yusuf Elsharawy", "Ella Krechmer", "Tomas Acuna", "Tina Nguyen", "Theo Fahey", "Sadid Ethun"
+]
+
+pd2 = ["Patrick Ging", "Wenhao Dong", "Raymond Yeung", "Kevin Cao", "Michael Borczuk", "Alif Abdullah", "Justin Zou", "Andy Lin", "Shadman Rakib", "David Chong","Daniel Sooknanan", "Cameron Nelson", "Austin Ngan", "Yaying Liang Li", "Eric Guo", "Noakai Aronesty", "Roshani Shrestha", "Yoonah Chang",
+    "Qina Liu", "Han Zhang", "Joshua Kloepfer"]
+
 
 def printRandomName(arr):
 
