eb840ebfed799ff1295407e5f2a19f17f4cacc85
cnelson20 <cjnelson2004@gmail.com>
2021-09-27 03:39:13 +0100

    added combined printer script

---
+++ 05_py/main.py
@@ -0,0 +1,24 @@
+# Cameron Nelson
+# SoftDev
+# HW - Amalgamate Random Student Name Printer
+# 2021-09-27
+
+import random
+
+# List people for periods 1 and 2
+pd1 = ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Ishraq Mahid", "Raymond Yeung", "Kevin Cao", "Ivan Lam", "Michelle Lo"]
+pd2 = ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee", "Patrick Ging", "Yaying Liang", "Shadman Rakib", "Cameron Nelson", "Eric Guo", "Daniel Sooknanen", "Andy Lin"]
+
+def printRandomName(arr):
+
+    #get random name from array
+    randomIndex = random.randrange(0, len(arr))
+    randomName = arr[randomIndex]
+    
+    print(randomName)
+
+def main(): 
+    printRandomName(pd1)
+    printRandomName(pd2)
+
+main();
