28914d88b9a4fb1333a898b1f3bd7f2c4cff12bb
cnelson20 <cjnelson2004@gmail.com>
2021-09-28 23:47:36 +0100

    added header

--- 06_py-csv/csv.py
+++ 06_py-csv/csv.py
@@ -1,25 +1,32 @@
+# Tigers - Cameron Nelson, Sophie Liu, Qina Liu
+# SoftDev
+# K06 - Reading & Parsing CSV file 
+# 2021-09-28
+#
+#
+
 import random
 
 file = open("occupations.csv");
 lines = file.read().split("\n");
+del lines[0];
 splitlines = [];
 listjobs = [];
 for i in lines:
     if "," in i:
         i = i.replace("\"","");
         comma = i.rsplit(",",1);
+        comma[1] = float(comma[1]);
         listjobs.append(comma[0]);
         splitlines.append(comma);
 
-del splitlines[0];
-del listjobs[0];
 del listjobs[len(listjobs)-1];
 jobs = dict(splitlines);
 
-randomNum = random.random() * float(jobs['Total']);
+randomNum = random.random() * jobs['Total'];
 for i in listjobs:
-    if randomNum < float(jobs[i]):
+    if randomNum < jobs[i]:
         print(i);
         break;
     else:
-        randomNum -= float(jobs[i]);
+        randomNum -= jobs[i];
