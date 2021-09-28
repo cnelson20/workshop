9966bb0e678907bcc2e83c7926967a5230ab1972
cnelson20 <cjnelson2004@gmail.com>
2021-09-28 23:39:31 +0100

    finished code

--- 06_py-csv/csv.py
+++ 06_py-csv/csv.py
@@ -3,15 +3,23 @@ import random
 file = open("occupations.csv");
 lines = file.read().split("\n");
 splitlines = [];
+listjobs = [];
 for i in lines:
     if "," in i:
         i = i.replace("\"","");
-        splitlines.append(i.rsplit(",",1));
+        comma = i.rsplit(",",1);
+        listjobs.append(comma[0]);
+        splitlines.append(comma);
 
 del splitlines[0];
-splitlines.pop();
+del listjobs[0];
+del listjobs[len(listjobs)-1];
 jobs = dict(splitlines);
 
-randomNum = random.random()*100;
-
-print(jobs);
+randomNum = random.random() * float(jobs['Total']);
+for i in listjobs:
+    if randomNum < float(jobs[i]):
+        print(i);
+        break;
+    else:
+        randomNum -= float(jobs[i]);
